b007176db69bcae6f2665ba8acbd2f6648fc33ad
Brad Mortensen <b.mort86@gmail.com>
2019-04-26 09:06:55 -0600

    parsing command line

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,7 +7,14 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
+  printf("There are %d command line argument(s):\n", argc);
+  if (argc <= 1)
+  {
+  }
+  for (int i = 0; i < argc; i++)
+  {
+    printf("   %s\n", argv[i]);
+  }
   // Open directory
 
   // Repeatly read and print entries
